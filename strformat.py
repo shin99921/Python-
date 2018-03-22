d77bc81732c07e34c47c77e6b707eadaef7085d6
shin99921 <sin99921@naver.com>
2018-03-22 16:16:30 +0900

    문자열 포멧에 관하여

---
+++ strformat.py
@@ -0,0 +1,24 @@
+print("I eat %d apple!"%3)
+
+print("I eat %s apple!"%"three")
+
+number=5
+print("I eat %d sushi"% number)
+
+day="six"
+print("I have %d book and %s pencil"% (number,day))
+
+flo=3.234
+print("This is float number test %d"% flo)
+#%d는 정수형이라서 실수로 출력하려면 실수형 %f
+print("This is float number test %f"% flo)
+
+#%를 출력하고 싶으면 %%를 사용한다.
+print("%f%%"% flo)
+
+print("%15s"%"hello")
+
+print("%0.9d"% 3)
+
+print("%0.9s"%"hello")#가능은 하지만 변화는 없다.
+
