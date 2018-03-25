0d7d5a744af5b2498cc7c185d1f5227a91f825f9
shin99921 <shin99921@naver.com>
2018-03-25 20:32:30 +0900

    숫자형

---
+++ number.py
@@ -0,0 +1,57 @@
+# 사칙연산
+print("1","-"*50)
+a=6
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print("\n")
+
+# 제곱
+print("2","-"*50)
+a=3
+b=4
+print(a**b)
+print(2**3)
+print(2**3**2)
+print("\n")
+
+# 정수형
+print("3","-"*50)
+a = 123
+b = -123
+c = 0
+print(a,b,c)
+print("\n")
+
+# 자료형
+print("4","-"*50)
+x=1
+y=2.123
+z="shin"
+
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print("\n")
+
+# 실수형
+print("5","-"*50)
+a = 1.2
+b = -2.34
+c = 4.24E10
+d = 4.22e-4
+print(a,b)
+print(c,d)
+print("\n")
+
+# 8진수와 16진수
+print("6","-"*50)
+a = 0o177
+b = 0x8ff
+c = 0xABC
+print(a,b,c)
+print("\n")
