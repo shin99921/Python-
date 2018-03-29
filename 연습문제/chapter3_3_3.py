7c0640b960325f13e9ea5e62a6e76cd7f8d91563
shin99921 <sin99921@naver.com>
2018-03-29 20:06:56 +0900

    챕터3연습문제

---
+++ 연습문제/chapter3_3_3.py
@@ -0,0 +1,32 @@
+"""
+사용자에게 태어난 년도를 입력받아 그 해의 띠를 출력하는 프로그램을 만드세요.
+"""
+# 힌트: 입력받은 년도를 12로 나눈 나머지를 사용합니다.
+
+year = int(input("년도를 입력하세요: "))
+y = year % 12
+
+if y == 0:
+    print("원숭이띠 입니다!")
+elif y == 1:
+    print("닭띠 입니다!")
+elif y == 2:
+    print("개띠 입니다!")
+elif y == 3:
+    print("돼지띠 입니다!")
+elif y == 4:
+    print("쥐띠 입니다!")
+elif y == 5:
+    print("소띠 입니다!")
+elif y == 6:
+    print("범띠 입니다!")
+elif y == 7:
+    print("토끼띠 입니다!")
+elif y == 8:
+    print("용띠 입니다!")
+elif y == 9:
+    print("뱀띠 입니다!")
+elif y == 10:
+    print("말띠 입니다!")
+else:
+    print("양띠 입니다!")
